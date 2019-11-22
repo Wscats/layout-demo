8d79671a7f91a0706c8785f723a724ae5de8dbd0
wscats <2587575267@qq.com>
2019-11-22 17:07:20 +0800

    test

--- push.lua
+++ push.lua
@@ -1,6 +1,7 @@
 print("Hello World!")
 print(os.execute('git add .'))
 print(os.execute('git commit -m "test"'))
+print(os.execute('git push origin master'))
 -- io.popen('git add .')
 -- io.popen('git commit -m "test"')
 -- local t = io.popen('git push origin master')
