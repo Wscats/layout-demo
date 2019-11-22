7e688bcb981d5aae1fa9c463ae5bbb1647e50e58
wscats <2587575267@qq.com>
2019-11-22 17:06:59 +0800

    test

--- push.lua
+++ push.lua
@@ -1,7 +1,9 @@
 print("Hello World!")
-io.popen('git add .')
-io.popen('git commit -m "test"')
-local t = io.popen('git push origin master')
-local a = t:read("*all")
-print(a)
+print(os.execute('git add .'))
+print(os.execute('git commit -m "test"'))
+-- io.popen('git add .')
+-- io.popen('git commit -m "test"')
+-- local t = io.popen('git push origin master')
+-- local a = t:read("*all")
+
 
